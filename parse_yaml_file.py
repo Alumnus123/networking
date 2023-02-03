4ea5de4b2d5e0aee7b58ec44f5fbc88800913751
Alumnus123 <puja.bag@alumnux.com>
2023-02-03 12:00:58 +0530

    2 files of updated parse_yaml_file.py

--- parse_yaml_file.py
+++ parse_yaml_file.py
@@ -8,3 +8,15 @@ def open_yaml_file():
         data = yaml.load(f, Loader=SafeLoader)
         print(data)
         return data
+
+
+# print(open_yaml_file())
+def access_yaml_file():
+    data = open_yaml_file()
+    mother_details = data['Family']['Maternal']['mother']
+    father_details = data['Family']['Paternal']['father']
+    new_data = {'mother': mother_details, 'father': father_details}
+    return new_data
+
+
+print(access_yaml_file())
